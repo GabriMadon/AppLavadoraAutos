ca9b4e0bdc4c7685803b006696f810e02734155d
juand <juand@192.168.10.5>
2024-08-25 18:49:05 -0500

    WIP

---
+++ src/main/java/Modelo/ConexionDB.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author juand
+ */
+//Class Conexion a db_lavadora_autos Mysql Workbrench
+public class ConexionDB {
+    //ATRIBUTIS
+    //statci variable de instancia // final = constante
+    private static final String URL ="jdbc:mysql://localhost:3306/db_lavadora_autos";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password123";
+    
+    //METODOS
+    public Connection getConnection(){
+        Connection connection = null;
+        try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(URL,USER,PASSWORD);
+            System.out.println("Conexxion exitosa a la base de datos");
+        }catch(SQLException| ClassNotFoundException e){
+            System.out.println("Error de conexion: " + e.getMessage());
+        }
+        return connection;
+    }
+    
+    //Main para probar conexio DB
+     public static void main(String[] args) {
+        ConexionDB db = new ConexionDB();
+        Connection conn = db.getConnection();
+        if (conn != null) {
+            try {
+                conn.close();
+                System.out.println("Conexión cerrada correctamente.");
+            } catch (SQLException e) {
+                System.out.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/main/java/iti/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package iti.main;
+
+import Vista.VentanaPrincipal;
+
+/**
+ *
+ * @author juand
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //Ver Ventana Principal 
+        VentanaPrincipal princ = new VentanaPrincipal();
+        princ.setVisible(true);//visible pantalla principal
+        princ.setLocationRelativeTo(null);// ubicacion en el centro de la pantalla
+    }
+}
